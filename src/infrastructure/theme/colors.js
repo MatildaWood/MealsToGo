a3f137f6fc6f1b164b6a4b0d30c2d09eef6957c0
MatildaWood <brenm002@gmail.com>
2021-12-24 13:39:33 +0000

    origin update

---
+++ src/infrastructure/theme/colors.js
@@ -0,0 +1,28 @@
+export const colors = {
+  brand: {
+    primary: "#241A1C",
+    secondary: "#483E40",
+    muted: "#7B6C6F",
+  },
+  ui: {
+    primary: "#291216",
+    secondary: "#757575",
+    tertiary: "#F1F1F1",
+    quaternary: "#AA999D",
+    disabled: "#DEDEDE",
+    error: "#D0421B",
+    success: "#138000",
+  },
+  bg: {
+    primary: "#AA999D",
+    secondary: "#F1F1F1",
+  },
+  text: {
+    primary: "#262626",
+    secondary: "#757575",
+    disabled: "#9C9C9C",
+    inverse: "#AA999D",
+    error: "#D0421B",
+    success: "#138000",
+  },
+};
